52ca16c17b72ce38d512c0f9bd65fe60758965fe
Panos Terzis <P.Terzis@ed.ac.uk>
2015-04-17 12:16:24 +0100

    Dtree upload was not working

--- src/js/former/jquery.former.dtree.js
+++ src/js/former/jquery.former.dtree.js
@@ -17,49 +17,45 @@ DTreeImplementation.prototype.implement = function(){
 
         $("#upload-button").unbind('click');
         $("#upload-button").click($.proxy(function(){
-                // TODO: When we migrate to modules get the sid & publicEditor from core
-                var dtreeFname = utils.getParams().sid + '.json';
-                var publicEditor = utils.getParams().public === 'true';
-                var options = {
-                    "remoteDir": "editors",
-                    "filename": dtreeFname,
-                    "file": file
+            // TODO: When we migrate to modules get the sid & publicEditor from core
+            var dtreeFname = utils.getParams().sid + '.json';
+            var publicEditor = utils.getParams().public === 'true';
+            var options = {
+                "remoteDir": "editors",
+                "path": dtreeFname,
+                "file": file,
+                "contentType": false
+            };
+
+            loading(true);
+            var target = this.target;
+
+            if(publicEditor){
+                options.urlParams = {
+                    'public': 'true'
                 };
+            }
 
-                loading(true);
-                var target = this.target;
-
-                if(publicEditor){
-                    options.urlParams = {
-                        'public': 'true'
-                    };
-                }
-
-                pcapi.uploadFile(options, $.proxy(function(result, data){
-                    if(result){
-                        $("#dialog-upload").dialog("close");
-                        alert("File was uploaded");
-                        $.ajax({
-                            url: "templates/dtreeTemplate.html",
-                            dataType: 'html',
-                            success: function(tmpl){
-                                var data = {
-                                    "i": 1,
-                                    "type": "dtree",
-                                    "title": file.name,
-                                    "dtree": 'editors/' + dtreeFname,
-                                    "url": pcapi.buildFSUrl('editors', dtreeFname)
-                                };
-                                var template = _.template(tmpl);
-                                $("#"+target).append(template(data));
-                            }
-                        });
+            pcapi.uploadFile(options).then($.proxy(function(result, data){
+                $("#dialog-upload").dialog("close");
+                alert("File was uploaded");
+                $.ajax({
+                    url: "templates/dtreeTemplate.html",
+                    dataType: 'html',
+                    success: function(tmpl){
+                        var data = {
+                            "i": 1,
+                            "type": "dtree",
+                            "title": file.name,
+                            "dtree": 'editors/' + dtreeFname,
+                            "url": pcapi.buildFSUrl('editors', dtreeFname)
+                        };
+                        var template = _.template(tmpl);
+                        $("#"+target).append(template(data));
                     }
-                    else{
-                        alert("There was an error");
-                    }
-                    loading(false);
-                }, this));
+                });
+                loading(false);
+            }, this));
         }, this));
     }
     else{
